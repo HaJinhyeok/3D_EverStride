ab9409b8b5e4032b6988e9cd52ef8bdc825c335d
HaJinhyeok <hjhmoon61@naver.com>
2025-04-09 00:59:02 +0900

    UI 작업중
    2025/04/09 00:58
    
    UI 작업 마무리

--- Assets/@Scripts/UI/PlayerHpBar.cs
+++ Assets/@Scripts/UI/PlayerHpBar.cs
@@ -5,21 +5,16 @@ using UnityEngine.UI;
 public class PlayerHpBar : MonoBehaviour
 {
     public Image PlayerHpImage;
-    public Text PlayerHpText;
     public static Action PlayerHpAction;
 
     void Start()
     {
-        //PlayerHpImage = GetComponentInChildren<Image>();
-        //PlayerHpText = GetComponentInChildren<Text>();
         PlayerHpAction += OnPlayerHpChanged;
         PlayerHpImage.fillAmount = GameManager.Instance.Player.PlayerHp / Define.PlayerMaxHp;
-        PlayerHpText.text = $"{GameManager.Instance.Player.PlayerHp} / {Define.PlayerMaxHp}";
     }
 
     void OnPlayerHpChanged()
     {
-        PlayerHpText.text = $"{GameManager.Instance.Player.PlayerHp} / {Define.PlayerMaxHp}";
         PlayerHpImage.fillAmount = GameManager.Instance.Player.PlayerHp / Define.PlayerMaxHp;
     }
 
